0eb41e06bcca84c1cbc2b5a682b931aa438e0f40
Le Quang Vu <leevudev@gmail.com>
2024-01-27 02:12:03 +0700

    [Fix] Disable scroll when modal is open

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,6 @@
-import React, { ReactNode } from "react";
+"use client";
+
+import React, { ReactNode, useEffect } from "react";
 import classNames from "classnames/bind";
 import styles from "./Modal.module.scss";
 
@@ -11,16 +13,17 @@ type Props = {
 };
 
 const Modal: React.FC<Props> = function ({ isShowing, toggle, children, transparent }: Props) {
-    if (isShowing) {
-        return (
-            <main className={cx("wrapper")}>
-                <section className={cx("modal")} onClick={toggle}></section>
-                {children}
-            </main>
-        );
-    }
+    useEffect(() => {
+        document.body.style.overflow = isShowing ? "hidden" : "auto";
+    }, [isShowing]);
 
-    return null;
+    if (!isShowing) return;
+    return (
+        <main className={cx("wrapper")}>
+            <section className={cx("modal")} onClick={toggle}></section>
+            {children}
+        </main>
+    );
 };
 
 export default Modal;
