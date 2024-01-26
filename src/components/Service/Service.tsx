9db60aa24e7b69005affc07a2c548a871447e6b7
Le Quang Vu <leevudev@gmail.com>
2024-01-27 04:41:49 +0700

    [Fix] Flip card 3D

--- src/components/Service/Service.tsx
+++ src/components/Service/Service.tsx
@@ -14,21 +14,25 @@ const cx = classNames.bind(styles);
 const Service = function ({ service, index }: Props) {
     return (
         <div className={cx("wrapper")} data-aos="fade-up" data-aos-delay={`${100 * (index + 4)}`} data-aos-duration={`${1000 * (index + 4)}`}>
-            <div className={cx("wrapper-inner")}>
-                <section className={cx("frontend")}>
-                    <div className={cx("service-icon")}>
-                        <Image className={cx("service-image")} src={service.image} alt="" />
+            <div className={cx("flip-card")}>
+                <div className={cx("flip-card-front")}>
+                    <div className={cx("inner")}>
+                        <div className={cx("service-icon")}>
+                            <Image className={cx("service-image")} src={service.image} alt="" />
+                        </div>
+                        <div className={cx("service-detail-front")}>
+                            <h3 className={cx("service-title")}>{service.title}</h3>
+                            <p className={cx("service-subtitle")}>{service.subTitle}</p>
+                        </div>
                     </div>
-                    <div className={cx("service-detail-front")}>
-                        <h3 className={cx("service-title")}>{service.title}</h3>
-                        <p className={cx("service-subtitle")}>{service.subTitle}</p>
+                </div>
+                <div className={cx("flip-card-back")}>
+                    <div className={cx("inner")}>
+                        <div className={cx("service-detail-back")}>
+                            <p className={cx("service-subtitle")}>{service.description}</p>
+                        </div>
                     </div>
-                </section>
-                <section className={cx("backend")}>
-                    <div className={cx("service-detail-back")}>
-                        <p className={cx("service-subtitle")}>{service.description}</p>
-                    </div>
-                </section>
+                </div>
             </div>
         </div>
     );
