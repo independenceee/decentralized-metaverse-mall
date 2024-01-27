5bdfb5ada617c4d1b1ee3ca276c691d0e34e6782
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-01-28 00:34:42 +0700

    feat: initial table component

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Table.module.scss";
+
+type Props = {};
+
+const Table = function ({}: Props) {
+    return <div></div>;
+};
+
+export default Table;
