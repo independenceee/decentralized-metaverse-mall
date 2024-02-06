e74f1e2e0ee2087c8f37d70d03045a659ec5b5ce
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2024-02-07 00:10:59 +0700

    develop

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -5,9 +5,9 @@ import React, { ReactNode, lazy } from "react";
 const LucidProvider = lazy(() => import("@/contexts/providers/LucidProvider"));
 const ModalProvider = lazy(() => import("@/contexts/providers/ModalProvider"));
 const WalletProvider = lazy(() => import("@/contexts/providers/WalletProvider"));
-const TransactionProvider = lazy(() => import("@/contexts/providers/TransactionProvider"));
 const StakeProvider = lazy(() => import("@/contexts/providers/StakeProvider"));
 const AccountProvider = lazy(() => import("@/contexts/providers/AccountProvider"));
+const TransactionProvider = lazy(() => import("@/contexts/providers/TransactionProvider"));
 
 type Props = {
     children: ReactNode;
